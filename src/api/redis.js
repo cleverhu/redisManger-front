75d60254a609895fd4d6c9012850014e517aa138
deeplythink <51436614+deeplythink@users.noreply.github.com>
2021-08-25 14:55:57 +0800

    Delete test.tar.gz

--- src/api/redis.js
+++ src/api/redis.js
@@ -1,57 +1,57 @@
-const CommonToFile = "http://redis.deeplythink.com/common/toFile"
-const CommonToMySql = "http://redis.deeplythink.com/common/toMySql"
+const CommonToFile = "http://redismanger.study-k8s.com/common/toFile"
+const CommonToMySql = "http://redismanger.study-k8s.com/common/toMySql"
 
-const StringScan = "http://redis.deeplythink.com/string/scan"    //method search cursor
-const StringDeleteByKeys = "http://redis.deeplythink.com/string/" //:keys
-const StringPost = "http://redis.deeplythink.com/string/"
-const StringToFile = "http://redis.deeplythink.com/string/toFile"
+const StringScan = "http://redismanger.study-k8s.com/string/scan"    //method search cursor
+const StringDeleteByKeys = "http://redismanger.study-k8s.com/string/" //:keys
+const StringPost = "http://redismanger.study-k8s.com/string/"
+const StringToFile = "http://redismanger.study-k8s.com/string/toFile"
 
-const ListScan = "http://redis.deeplythink.com/list/scan"    //method search cursor
-const ListGetByKey = "http://redis.deeplythink.com/list/get/" //:key
-const ListDeleteByKeys = "http://redis.deeplythink.com/list/" //:keys
-const ListPost = "http://redis.deeplythink.com/list/" //
-const ListExists = "http://redis.deeplythink.com/list/exists/" //:id
-const ListRemove = "http://redis.deeplythink.com/list/remove" //
-const ListInsert = "http://redis.deeplythink.com/list/insert" //
+const ListScan = "http://redismanger.study-k8s.com/list/scan"    //method search cursor
+const ListGetByKey = "http://redismanger.study-k8s.com/list/get/" //:key
+const ListDeleteByKeys = "http://redismanger.study-k8s.com/list/" //:keys
+const ListPost = "http://redismanger.study-k8s.com/list/" //
+const ListExists = "http://redismanger.study-k8s.com/list/exists/" //:id
+const ListRemove = "http://redismanger.study-k8s.com/list/remove" //
+const ListInsert = "http://redismanger.study-k8s.com/list/insert" //
 
-const HashScan = "http://redis.deeplythink.com/hash/scan"
-const HashGetByKey = "http://redis.deeplythink.com/hash/get/" //:key
-const HashDeleteByKeys = "http://redis.deeplythink.com/hash/" //:keys
-const HashRemove = "http://redis.deeplythink.com/hash/remove" //
-const HashPost = "http://redis.deeplythink.com/hash/" //
-
-
-const SetScan = "http://redis.deeplythink.com/set/scan"    //method search cursor
-const SetGetByKey = "http://redis.deeplythink.com/set/get/" //:key
-const SetPost = "http://redis.deeplythink.com/set/"
-const SetDeleteByKeys = "http://redis.deeplythink.com/set/" //:keys
-const SetRemove = "http://redis.deeplythink.com/set/remove" //
-const SetGetCommon = "http://redis.deeplythink.com/set/getCommon" //
-
-const ZSetScan = "http://redis.deeplythink.com/sortedSet/scan"
-const ZSetDeleteByKeys = "http://redis.deeplythink.com/sortedSet/" //:keys
-const ZSetGetByKey = "http://redis.deeplythink.com/sortedSet/get/" //:key
-const ZSetPost = "http://redis.deeplythink.com/sortedSet/"
-const ZSetRemove = "http://redis.deeplythink.com/sortedSet/remove" //
-
-const GEOScan = "http://redis.deeplythink.com/geo/scan"
-const GEOPost = "http://redis.deeplythink.com/geo/"
-const GEODeleteByKeys = "http://redis.deeplythink.com/geo/" //:keys
+const HashScan = "http://redismanger.study-k8s.com/hash/scan"
+const HashGetByKey = "http://redismanger.study-k8s.com/hash/get/" //:key
+const HashDeleteByKeys = "http://redismanger.study-k8s.com/hash/" //:keys
+const HashRemove = "http://redismanger.study-k8s.com/hash/remove" //
+const HashPost = "http://redismanger.study-k8s.com/hash/" //
 
 
-const ConfigGet = "http://redis.deeplythink.com/config/"         //get all
-const ConfigPost = "http://redis.deeplythink.com/config/"
+const SetScan = "http://redismanger.study-k8s.com/set/scan"    //method search cursor
+const SetGetByKey = "http://redismanger.study-k8s.com/set/get/" //:key
+const SetPost = "http://redismanger.study-k8s.com/set/"
+const SetDeleteByKeys = "http://redismanger.study-k8s.com/set/" //:keys
+const SetRemove = "http://redismanger.study-k8s.com/set/remove" //
+const SetGetCommon = "http://redismanger.study-k8s.com/set/getCommon" //
 
-const InfoGet = "http://redis.deeplythink.com/info/"
+const ZSetScan = "http://redismanger.study-k8s.com/sortedSet/scan"
+const ZSetDeleteByKeys = "http://redismanger.study-k8s.com/sortedSet/" //:keys
+const ZSetGetByKey = "http://redismanger.study-k8s.com/sortedSet/get/" //:key
+const ZSetPost = "http://redismanger.study-k8s.com/sortedSet/"
+const ZSetRemove = "http://redismanger.study-k8s.com/sortedSet/remove" //
 
-const UserLogin = "http://redis.deeplythink.com/user/login"
-const UserCheck = "http://redis.deeplythink.com/user/check"
+const GEOScan = "http://redismanger.study-k8s.com/geo/scan"
+const GEOPost = "http://redismanger.study-k8s.com/geo/"
+const GEODeleteByKeys = "http://redismanger.study-k8s.com/geo/" //:keys
 
-const ConnectTest = "http://redis.deeplythink.com/connect/test"
-const ConnectSave = "http://redis.deeplythink.com/connect/save"
-const ConnectGet = "http://redis.deeplythink.com/connect/get"
 
-const LogsGet = "http://redis.deeplythink.com/logs/get"
+const ConfigGet = "http://redismanger.study-k8s.com/config/"         //get all
+const ConfigPost = "http://redismanger.study-k8s.com/config/"
+
+const InfoGet = "http://redismanger.study-k8s.com/info/"
+
+const UserLogin = "http://redismanger.study-k8s.com/user/login"
+const UserCheck = "http://redismanger.study-k8s.com/user/check"
+
+const ConnectTest = "http://redismanger.study-k8s.com/connect/test"
+const ConnectSave = "http://redismanger.study-k8s.com/connect/save"
+const ConnectGet = "http://redismanger.study-k8s.com/connect/get"
+
+const LogsGet = "http://redismanger.study-k8s.com/logs/get"
 
 
 export default {
